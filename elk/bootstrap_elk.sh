4bd7d2b61537deae072de2d246f64452defecc2e
=IdoBaram <idobaram1990@gmail.com>
2020-04-20 07:10:10 +0300

    update elk.sh

--- elk/bootstrap_elk.sh
+++ elk/bootstrap_elk.sh
@@ -7,32 +7,32 @@ ELK_VERSION="7.6.0"
 # update apt
 apt-get update --quiet
 apt-get install -y unzip ifupdown git apt-transport-https default-jre --quiet
-
+echo "done apt"
 java -version
 [ -z $JAVA_HOME ] && echo "JAVA_HOME not set" || echo "JAVA_HOME is ${JAVA_HOME}"
-
+echo "done java"
 # install the Elastic PGP Key and repo
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
-
+echo "done Elastic PGP Key and repo"
 apt-get install --quiet -y elasticsearch=$ELK_VERSION 
-
+echo "done elk"
 # copy over configs
 cp /home/ubuntu/elk/elk/configs/elasticsearch/elasticsearch.yml /etc/elasticsearch/
 /bin/systemctl daemon-reload
 /bin/systemctl enable elasticsearch.service
 /bin/systemctl start elasticsearch.service
-
+echo "done copy over configs"
 # Install Kibana
 apt-get install --quiet -y kibana=$ELK_VERSION 
-
+echo "done Install Kibana"
 # copy over configs
 cp /home/ubuntu/elk/elk/configs/kibana/kibana.yml /etc/kibana/
-
+echo "done copy over configs"
 /bin/systemctl daemon-reload
 /bin/systemctl enable kibana.service
 systemctl start kibana.service
-
+echo "done copy over configs"
 
 # install Logstash
 apt-get install  --quiet -y logstash=1:$ELK_VERSION-1 
@@ -41,8 +41,10 @@ apt-get install --quiet -y filebeat
 #cp -R /home/ubuntu/elk/elk/configs/logstash/* /etc/logstash/conf.d/
 #systemctl enable logstash.service
 #systemctl start logstash.service
-
+echo "done install Logstash"
 
 # Smoke tests
 netstat -pant
 curl -XGET -s http://localhost:9200
+
+
